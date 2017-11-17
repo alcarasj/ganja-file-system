0a55cfed68ac719a29a995d1d8222d3cefe32b05
Jerico Alcaras <alcarasj@tcd.ie>
2017-11-17 20:48:39 +0000

    Began read/write endpoints for file server.

--- file-server/file-server.js
+++ file-server/file-server.js
@@ -3,28 +3,48 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const fileUpload = require('express-fileupload');
 
 const PORT = 8080;
 const STUDENT_NUMBER = 14317110;
-const DATADIR = './data';
+const DIR = './data';
+const DATADIR = '/data/';
 
 const fileServer = express();
+fileServer.use(fileUpload());
 
-if (!fs.existsSync(DATADIR)) {
-    fs.mkdirSync(DATADIR);
+if (!fs.existsSync(DIR)) {
+    fs.mkdirSync(DIR);
 }
 
-fileServer.get('/write', (request, response) => {
-  response.send('Hello from Express!')
-})
+fileServer.post('/write', (request, response) => {
+  if (!request.files) {
+     return response.status(400).send('No files were uploaded.');
+  } else {
+    var fileName = request.param('fileName');
+    if (fs.existsSync(DATADIR + fileName)) {
+      console.log('Found file.');
+      //Overwrite
+    } else {
+      //Create
+      fs.writeFile(DATADIR + fileName, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+        });
+    }
+  }
+});
 
 fileServer.get('/read', (request, response) => {
   response.sendFile('test.jpg', { root: path.join(__dirname, './data') });
-})
+});
 
 fileServer.listen(PORT, (err) => {
   if (err) {
     return console.log('Something bad happened.', err)
   }
   console.log(`File server listening on 0.0.0.0:${PORT}`)
-})
+});

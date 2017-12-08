a5aaf5086266a02bf7ef4227fcfd6bd150646f22
Jerico Alcaras <alcarasj@tcd.ie>
2017-12-08 14:38:59 +0000

    Lock server can now lock and check lock on files.

--- ganja-lock-server/lockServer.js
+++ ganja-lock-server/lockServer.js
@@ -12,36 +12,85 @@ const formidable = require('formidable');
 const querystring = require("querystring");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const Cacheman = require('cacheman');
 
 const PORT = 8071;
 const TMPDIR = './tmp';
 const AUTH_SERVER = '127.0.0.1:8070';
 //This secret is just used for testing purposes. In production, use environment variable.
 const SECRET = 'yallmothafuckasneedjesus';
+const MAX_LOCK_TIME = 86400;
+const DEFAULT_LOCK_TIME = 60;
+const LOCKLOG = '[LOCK] ';
+const UNLOCKLOG = '[UNLOCK] ';
 
 if (!fs.existsSync(TMPDIR)) {
   fs.mkdirSync(TMPDIR);
 }
 
-//TODO Implement cache-style locking service.
-
+var fileLocks = new Cacheman({ ttl: DEFAULT_LOCK_TIME });
 var lockServer = express();
 lockServer.use(bodyParser.urlencoded({ extended: false }));
 lockServer.use(bodyParser.json());
 
-webServer.get('/lock', (req, res) => {
-  const clientLog = "[" + req.ip + "] ";
-  //TODO
+lockServer.get('/lock', (req, res) => {
+  const fileName = req.query.fileName;
+  const lockTimeInSeconds = req.query.lockTimeInSeconds ? parseInt(req.query.lockTimeInSeconds) : DEFAULT_LOCK_TIME;
+  fileLocks.get(fileName, (err, value) => {
+    if (err) {
+      console.error(err);
+      return res.sendStatus(500);
+    }
+    if (value) {
+      return res.status(400).send({ success: false, message: "This file has already been locked."});
+    } else {
+      if (lockTimeInSeconds && lockTimeInSeconds > 86400) {
+        return res.status(400).send({ success: false, message: "Lock time must not exceed " + MAX_LOCK_TIME + " seconds."});
+      }
+      fileLocks.set(fileName, { locked: true }, lockTimeInSeconds, (err, value) => {
+        if (err) {
+          console.error(err);
+          return res.sendStatus(500);
+        }
+        if (value) {
+          console.log(LOCKLOG + lockTimeInSeconds + " seconds on " + fileName);
+          return res.status(200).send({ success: true, message: "Lock granted for " + fileName + " for " + (lockTimeInSeconds ? lockTimeInSeconds : DEFAULT_LOCK_TIME) + " seconds."});
+        } else {
+          return res.sendStatus(400);
+        }
+      });
+    }
+  });
 });
 
-webServer.get('/unlock', (req, res) => {
-  const clientLog = "[" + req.ip + "] ";
-  //TODO
+lockServer.get('/unlock', (req, res) => {
+  const fileName = req.query.fileName;
+  fileLocks.get(fileName, (err, value) => {
+    if (err) {
+      console.error(err);
+      return res.sendStatus(500);
+    }
+    if (value) {
+      //delete entry
+    } else {
+      return res.status(400).send({ success: false, message: "There is no lock on this file."});
+    }
+  });
 });
 
-webServer.get('/checkForLock', (req, res) => {
-  const clientLog = "[" + req.ip + "] ";
-  //TODO
+lockServer.get('/checkForLock', (req, res) => {
+  const fileName = req.query.fileName;
+  fileLocks.get(fileName, (err, value) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log(value);
+    if (value) {
+      return res.status(200).send(value);
+    } else {
+      return res.status(200).send({ locked: false });
+    }
+  });
 });
 
 lockServer.listen(PORT, (err) => {
